e8c4146950535d7b2f17bde29a77cd5052239e50
ymdman <revitalized@icloud.com>
2020-12-06 17:01:00 +0900

    fix: ブログページの整形とクエリの変更

--- src/pages/blogpost.tsx
+++ src/pages/blogpost.tsx
@@ -1,28 +1,33 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { ContentfulBlogPost } from "../../graphql-types"
+import React from 'react';
+import { graphql } from 'gatsby';
+import { ContentfulBlogPost } from '../../graphql-types';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 type BlogPost = {
-  contentfulBlogPost: ContentfulBlogPost
-}
+  contentfulBlogPost: ContentfulBlogPost;
+};
 
 const blogPostPage: React.FC<{ data: BlogPost }> = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>{ data.contentfulBlogPost.title }</h1>
-    <time>{ data.contentfulBlogPost.publishDate }</time>
+    <h1>{data.contentfulBlogPost.title}</h1>
+    <time>{data.contentfulBlogPost.publishDate}</time>
     <ul>
-      { data.contentfulBlogPost.category?.map(item => (
-        <li key={ item?.id }>{ item?.name }</li>
-      )) }
+      {data.contentfulBlogPost.category?.map(item => (
+        <li key={item?.id}>{item?.name}</li>
+      ))}
     </ul>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sed ad harum culpa exercitationem maxime veritatis sunt cupiditate eaque, omnis reprehenderit nostrum reiciendis, temporibus dolore voluptates ullam magni tempore quis.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sed ad
+      harum culpa exercitationem maxime veritatis sunt cupiditate eaque, omnis
+      reprehenderit nostrum reiciendis, temporibus dolore voluptates ullam magni
+      tempore quis.
+    </p>
     <p>Now go build something great.</p>
   </Layout>
-)
+);
 
 export const query = graphql`
   query {
@@ -36,6 +41,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default blogPostPage
+export default blogPostPage;

456f2cdab06056d8112894c9a40252e2dc6cb546
ymdman <revitalized@icloud.com>
2021-01-05 08:05:54 +0900

    refactor: タイトルのデザイン修正

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import { fontSize } from '../styles/settings';
+import { fontSize, layout } from '../styles/settings';
 
 type Label = {
   label?: string | null;
@@ -14,23 +14,11 @@ const heading = css`
   font-weight: bold;
   word-break: break-all;
 
-  &::before,
-  &::after {
-    content: '';
-    flex-shrink: 0;
-    height: 1px;
-    width: 20px;
-    background-color: #ccc;
-  }
-
-  &::before {
-    margin-right: 15px;
-  }
-
-  &::after {
-    margin-left: 15px;
+  @media screen and (max-width: ${layout.threshold}px) {
+    font-size: ${fontSize.xxxLarge};
   }
 `;
+
 const Heading: React.FC<Label> = ({ label }) => {
   return <h1 css={heading}>{label}</h1>;
 };

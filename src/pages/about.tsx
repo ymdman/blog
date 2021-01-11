a1c89825d8485fa4137088a78bea3a1476ed72a0
ymdman <revitalized@icloud.com>
2021-01-11 14:59:27 +0900

    update: aboutページをリッチテキスト→マークダウンに変更

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { ContentfulAboutConnection } from '../../graphql-types';
+import { ContentfulAbout } from '../../graphql-types';
 import Layout from '../components/layout';
 import Heading from '../components/heading';
 import Body from '../components/body';
 import Section from '../components/section';
 import SectionHeading from '../components/sectionHeading';
 import SEO from '../components/seo';
-import { css } from '@emotion/react';
-import { layout } from '../styles/settings';
+// import { css } from '@emotion/react';
+// import { layout } from '../styles/settings';
 
-type About = {
-  data: {
-    allContentfulAbout: ContentfulAboutConnection;
+type ChildMarkdownRemark = {
+  childMarkdownRemark: {
+    html: string;
   };
 };
 
-const text = css`
-  margin-top: 15px;
+type MarkdownRemark = {
+  site: ChildMarkdownRemark;
+  profile: ChildMarkdownRemark;
+};
 
-  @media screen and (max-width: ${layout.threshold}px) {
-    margin-top: 10px;
-  }
-`;
+type About = {
+  data: {
+    contentfulAbout: ContentfulAbout & MarkdownRemark;
+  };
+};
 
 const About: React.FC<About> = ({ data }) => {
-  const node = data.allContentfulAbout.edges[0].node;
-
   return (
     <Layout>
       <SEO title="About" />
@@ -34,11 +35,19 @@ const About: React.FC<About> = ({ data }) => {
       <Body>
         <Section>
           <SectionHeading label={'This Site'} />
-          <p css={text}>{node.site?.internal.content}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data.contentfulAbout.site?.childMarkdownRemark?.html,
+            }}
+          ></div>
         </Section>
         <Section>
           <SectionHeading label={'Profile'} />
-          <p css={text}>{node.profile?.internal.content}</p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: data.contentfulAbout.profile?.childMarkdownRemark?.html,
+            }}
+          ></div>
         </Section>
       </Body>
     </Layout>
@@ -47,19 +56,15 @@ const About: React.FC<About> = ({ data }) => {
 
 export const query = graphql`
   query {
-    allContentfulAbout {
-      edges {
-        node {
-          profile {
-            internal {
-              content
-            }
-          }
-          site {
-            internal {
-              content
-            }
-          }
+    contentfulAbout {
+      profile {
+        childMarkdownRemark {
+          html
+        }
+      }
+      site {
+        childMarkdownRemark {
+          html
         }
       }
     }

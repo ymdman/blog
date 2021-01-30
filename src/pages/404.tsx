f1f69eae520f9fe685c4e3aadc1b3edd2e405ee9
ymdman <revitalized@icloud.com>
2021-01-30 14:04:26 +0900

    refactor: テキストの位置調整

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { css } from '@emotion/react';
+
+const container = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+const title = css`
+  font-size: 8rem;
+  font-weight: bold;
+  line-height: 1;
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 Not Found" />
+    <div css={container}>
+      <h1 css={title}>404</h1>
+      <p>Not Found</p>
+    </div>
   </Layout>
 );
 

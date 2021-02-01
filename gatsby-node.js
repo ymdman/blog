fc7d0559a1917797ba9920cb675b3d827c8267a7
ymdman <revitalized@icloud.com>
2021-02-02 00:13:20 +0900

    fix: ビルド時のwindow is not definedエラーを解消

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,3 +61,18 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};

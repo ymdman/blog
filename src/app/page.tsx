cb3fa737403cc3ebdfcfe875457d030a0019c33b
ymdman <revitalized@icloud.com>
2024-03-26 00:12:18 +0900

    Panda CSSを使用できるように

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import { css } from '../../styled-system/css';
 
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
+      Hello 🐼!
+    </div>
+  );
 }

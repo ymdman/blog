71eaca70938d0e6eda2979e50a5941fc4874a485
ymdman <revitalized@icloud.com>
2024-05-04 10:55:52 +0900

    tsparticlesを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { css } from '../../styled-system/css';
+import { Stack } from '../../styled-system/jsx';
+import { ParticlesBackground } from '../components/ParticlesBackground';
 
 export default function Home() {
-  return <div className={styles}>Hello 🐼!</div>;
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      height="calc(100% - 40px)"
+    >
+      <p className={css({ fontSize: '5xl', fontWeight: 'bold' })}>
+        Welcome to this site.
+      </p>
+      <ParticlesBackground />
+    </Stack>
+  );
 }
-
-const styles = css({
-  fontWeight: { base: 'normal', desktop: 'bold' },
-});

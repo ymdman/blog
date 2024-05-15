529a90dbcca182a414e7f6d6d07d8e1375f690ee
ymdman <revitalized@icloud.com>
2024-05-15 23:53:41 +0900

    Apollo Clientを入れた

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,32 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+'use client';
 
-export default function Home() {
-  return <div>about</div>;
+import { gql } from '@apollo/client';
+import createApolloClient from '../../../apollo-client';
+
+export async function getData() {
+  const client = createApolloClient();
+  const { data } = await client.query({
+    query: gql`
+      query About {
+        about(id: "") {
+          profile
+          site
+        }
+      }
+    `,
+  });
+
+  console.log(data);
+
+  return {
+    props: {
+      about: data,
+    },
+  };
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return <main>Lorem ipsum dolor sit amet.</main>;
 }

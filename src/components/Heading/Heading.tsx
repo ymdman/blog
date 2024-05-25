5778c56074ec204cc184b3c42a8e9acf17af21e1
ymdman <revitalized@icloud.com>
2024-05-25 09:42:10 +0900

    Headingを追加

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+import { css } from '../../../styled-system/css';
+
+type Elm = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+type HeadingProps = {
+  as: Elm;
+  children: React.ReactNode;
+};
+
+export function Heading({ as: CustomTag, children }: HeadingProps) {
+  return (
+    <CustomTag className={css({ fontWeight: 'bold' })}>{children}</CustomTag>
+  );
+}
